1912312a71fb57dba6325e7f35d430ca4735e1de
Fernando Méndez <47863038+mendez-jfer@users.noreply.github.com>
2021-11-12 14:14:56 -0600

    Update AzureConfig.py

--- AzureConfig.py
+++ AzureConfig.py
@@ -1,8 +1,8 @@
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
-KEYS = ['e05f24b6835c43a1bdc47bc056f808ce', '']
-ENDPOINTS = ['https://cognitiveps.cognitiveservices.azure.com/', '']
+KEYS = ['', '']
+ENDPOINTS = ['', '']
 
 
 def textAnalyticsKey(pos):
